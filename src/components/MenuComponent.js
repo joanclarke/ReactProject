2150e6571c457493f8e43565a41e3be4598acbc8
Joan Clarke <joanclarke100@gmail.com>
2018-11-21 15:28:20 -0500

    React component: Lifecycle methods part 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,6 +7,11 @@ class Menu extends Component {
         this.state = {    
           selectedDish: null 
         }
+        console.log('Menu Component constructor is invoked');
+    }
+
+    componentDidMount() {
+      console.log('Menu Component componentDidMount is invoked');
     }
 
     onDishSelect(dish) {
@@ -45,6 +50,9 @@ class Menu extends Component {
               </div>
             );
         });
+
+        console.log('Mwnu Component render is invoked');
+
         return (
           <div className="container">
             <div className="row">
